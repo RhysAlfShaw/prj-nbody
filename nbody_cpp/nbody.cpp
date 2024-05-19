9a92ef72bcaa9df1fb2cc675128bd990f28cfbf1
Rhys Shaw <45737158+RhysAlfShaw@users.noreply.github.com>
2024-05-19 19:08:29 +0100

    update

--- nbody_cpp/nbody.cpp
+++ nbody_cpp/nbody.cpp
@@ -4,15 +4,28 @@
 #include <fstream>
 #include <string>
 
+namespace std;
+
 const double G = 6.67430e-11; // Gravitational constant
 const double Msol = 1.989e30; // Solar mass
 const double AU = 1.496e11;   // Astronomical unit
+
+struct BodyT {
+    vector<double> x, y, z;      // Position
+    vector<double> vx, vy, vz;   // Velocity
+    vector<double> mass;         // Mass
+    <vector<double> t;         // Acceleration
+};
 
 struct Body {
     double x, y, z;      // Position
     double vx, vy, vz;   // Velocity
     double mass;         // Mass
 };
+
+
+
+
 
 // Function to calculate the acceleration due to gravity
 std::vector<double> acceleration(const std::vector<Body>& bodies, int i) {
@@ -32,74 +45,61 @@ std::vector<double> acceleration(const std::vector<Body>& bodies, int i) {
     return acc;
 }
 
+vector<double> integrate(int i, double dt, vector<Body>& bodies) {
+
+    acc = acceleration(bodies, i);
+    bodies[i].vx += acc[0] * dt / 2;
+    bodies[i].vy += acc[1] * dt / 2;
+    bodies[i].vz += acc[2] * dt / 2;
+
+    bodies[i].x += bodies[i].vx * dt;
+    bodies[i].y += bodies[i].vy * dt;
+    bodies[i].z += bodies[i].vz * dt;
+
+    acc = acceleration(bodies, i);
+    bodies[i].vx += acc[0] * dt / 2;
+    bodies[i].vy += acc[1] * dt / 2;
+    bodies[i].vz += acc[2] * dt / 2;
+
+    return bodies;
+}
+
+Body center_of_mass(const std::vector<Body>& bodies) {
+    double x = 0, y = 0, z = 0, m = 0;
+    for (int i = 0; i < bodies.size(); i++) {
+        x += bodies[i].mass * bodies[i].x;
+        y += bodies[i].mass * bodies[i].y;
+        z += bodies[i].mass * bodies[i].z;
+        m += bodies[i].mass;
+    }
+    return {x / m, y / m, z / m, 0, 0, 0, m};
+}
+
+struct Body run_simulation(const std::vector<Body>& bodies, double dt, double tmax) {
+    double time = 0;
+    for (double t = 0; t < tmax; t += dt) {
+        for (int i = 0; i < bodies.size(); i++) {
+            bodies = integrate(i, dt, bodies);
+        }
+        Body com = center_of_mass(bodies);
+
+        // save the Body data into the BodyT struct
+        BodyT bodyt;
+        bodyt.x.push_back(com.x);
+        bodyt.y.push_back(com.y);
+        bodyt.z.push_back(com.z);
+        bodyt.vx.push_back(com.vx);
+        bodyt.vy.push_back(com.vy);
+        bodyt.vz.push_back(com.vz);
+        bodyt.mass.push_back(com.mass);
+    }
+    return bodies;
+}
+
+
 int main() {
-    std::vector<Body> bodies;
-    std::string filename;
-    std::cout << "Enter the input file name: ";
-    std::cin >> filename;
-
-    std::ifstream file(filename);
-    if (!file) {
-        std::cerr << "Error: Unable to open file " << filename << "\n";
-        return 1;
-    }
-
-    int n;
-    file >> n;
-
-    bodies.resize(n);
-
-    double dt, Tend;
-    for (int i = 0; i < n; i++) {
-        file >> bodies[i].x >> bodies[i].y >> bodies[i].z
-             >> bodies[i].vx >> bodies[i].vy >> bodies[i].vz
-             >> bodies[i].mass >> dt >> Tend;
-    }
     
-    // Convert the initial positions and velocities to SI units
-    for (int i = 0; i < n; i++) {
-        bodies[i].x *= AU;
-        bodies[i].y *= AU;
-        bodies[i].z *= AU;
-        bodies[i].mass *= Msol;
-    }
-
-
-    std::ofstream output_file("output.txt");
-
-    double t = 0.0;
-    while (t < Tend) {
-        for (int i = 0; i < n; i++) {
-            std::vector<double> acc = acceleration(bodies, i);
-            bodies[i].vx += acc[0] * dt / 2.0;
-            bodies[i].vy += acc[1] * dt / 2.0;
-            bodies[i].vz += acc[2] * dt / 2.0;
-        }
-
-        for (int i = 0; i < n; i++) {
-            bodies[i].x += bodies[i].vx * dt;
-            bodies[i].y += bodies[i].vy * dt;
-            bodies[i].z += bodies[i].vz * dt;
-        }
-
-        for (int i = 0; i < n; i++) {
-            std::vector<double> acc = acceleration(bodies, i);
-            bodies[i].vx += acc[0] * dt / 2.0;
-            bodies[i].vy += acc[1] * dt / 2.0;
-            bodies[i].vz += acc[2] * dt / 2.0;
-        }
-
-        t += dt;
-
-        for (int i = 0; i < n; i++) {
-            output_file << t << " " << bodies[i].x << " " << bodies[i].y << " " << bodies[i].z << " "
-                        << bodies[i].vx << " " << bodies[i].vy << " " << bodies[i].vz << "\n";
-        }
-    }
-
-    file.close();
-    output_file.close();
-    std::cout << "Simulation completed. Results saved in output.txt.\n";
+    printf("Hello, World!\n");
 
     return 0;
 }
